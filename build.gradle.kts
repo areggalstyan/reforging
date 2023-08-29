1a646a686e75a574d205dc6c551509c004c96663
areggalstyan <areg.galstyan.levon@gmail.com>
2023-08-29 23:55:43 +0400

    Add support for older versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 group = "com.aregcraft"
-version = "5.9.0"
+version = "5.10.0"
 
 repositories {
     mavenLocal()
@@ -13,7 +13,7 @@ repositories {
 
 dependencies {
     compileOnly("com.aregcraft.delta:meta:1.0.0")
-    compileOnly("org.spigotmc:spigot-api:1.20.1-R0.1-SNAPSHOT")
+    compileOnly("org.spigotmc:spigot-api:1.16.5-R0.1-SNAPSHOT")
     implementation("com.aregcraft.delta:api:1.0.0")
     implementation("net.objecthunter:exp4j:0.4.8")
     implementation("org.bstats:bstats-bukkit:3.0.1")
@@ -21,7 +21,7 @@ dependencies {
 
 pluginDescription {
     main.set("$group.reforging.Reforging")
-    apiVersion.set("1.19")
+    apiVersion.set("1.16")
     load.set("STARTUP")
     commands {
         create("reforge") {
